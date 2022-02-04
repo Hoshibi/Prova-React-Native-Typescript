239b39ff91e6bfd711e457d793a1a33a8ede07c3
Hoshibi <tanyshiotani@hotmail.com>
2022-02-04 10:12:06 -0300

    Loteria 3 - Ver.14
    - Component: CartCard (component da página Cart)

---
+++ src/components/CartCard/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container, BtnTrash, NumbersContainer, Numbers, InfoNumbersContainer, GameType, Price } from "./styles";
+
+const CartCard: React.FC = () => {
+    return (
+        <Container>
+            <BtnTrash>
+
+            </BtnTrash>
+            <NumbersContainer>
+                <Numbers>01, 02, 04, 05, 06, 07, 09, 15, 17, 20, 21, 22, 23, 24, 25</Numbers>
+                <InfoNumbersContainer>
+                    <GameType>Lotofácil</GameType>
+                    <Price>R$ 2,50</Price>
+                </InfoNumbersContainer>
+            </NumbersContainer>
+        </Container>
+    );
+}
+
+export default CartCard;
---
+++ src/components/CartCard/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View `
+    margin-bottom: 25px;
+`;
+
+export const BtnTrash = styled.View `
+
+`;
+
+export const NumbersContainer = styled.View `
+    border-radius: 5px;
+    border-style: solid;
+    border-left-width: 5px;
+    border-color: #7F3992;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 15px;
+`;
+
+export const Numbers = styled.Text `
+    font-family: 'WorkSans_600SemiBold_Italic';
+    font-size: 18px;
+    color: #707070;
+`;
+
+export const InfoNumbersContainer = styled.View `
+    display: flex;
+    flex-direction: row;
+    margin-top: 7px;
+`;
+
+export const GameType = styled.Text `
+    margin-right: 15px;
+    font-family: 'WorkSans_600SemiBold_Italic';
+    font-size: 18px;
+    color: #7F3992;
+`;
+
+export const Price = styled.Text `
+    font-family: 'WorkSans_400Regular';
+    font-size: 18px;
+    color: #707070;
+`;
+
